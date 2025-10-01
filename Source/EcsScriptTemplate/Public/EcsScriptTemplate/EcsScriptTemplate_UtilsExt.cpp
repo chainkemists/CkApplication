35ad534b62c0c52038e5039b6ff9a4db04415a9a
Saad K <saadrustam@gmail.com>
2025-10-01 07:20:51 -0700

    chore: removed the now deprecated EcsScript template

--- Source/EcsScriptTemplate/Public/EcsScriptTemplate/EcsScriptTemplate_UtilsExt.cpp
+++
@@ -1,7 +0,0 @@
-#include "EcsScriptTemplate_UtilsExt.h"
-
-#include "EcsScriptTemplate/EcsScriptTemplate_Utils.h"
-
-// --------------------------------------------------------------------------------------------------------------------
-
-CK_INTENTIONALLY_EMPTY()
